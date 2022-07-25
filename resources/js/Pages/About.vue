1579e59fcc0c4174efd75442c560e25fc34aaa42
salman608 <salman608bd@gmail.com>
2022-07-25 14:41:00 +0600

    laravel with inartia setup

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,8 @@
+<template>
+
+    <h1>welcome To about</h1>
+    <inertia-link href="/"></inertia-link>
+    <inertia-link href="/about"></inertia-link>
+    <inertia-link href="/contact"></inertia-link>
+
+</template>
---
+++ resources/js/Pages/Contact.vue
@@ -0,0 +1,8 @@
+<template>
+
+    <h1>welcome To Contact</h1>
+    <inertia-link href="/"></inertia-link>
+    <inertia-link href="/about"></inertia-link>
+    <inertia-link href="/contact"></inertia-link>
+
+</template>
